e71bf734246fb9a2f5d794130e10cc3c49aebc2c
Ruslan Symonenko <symonenkors@gmail.com>
2024-06-20 17:59:56 +0300

    added basic settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { CLIENT_URL } from './consts/client';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    HttpModule.register({
+      baseURL: CLIENT_URL,
+      withCredentials: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
