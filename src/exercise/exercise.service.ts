f123b714b6c50cb12f83f57d8c8181b357aa97aa
Ruslan Symonenko <symonenkors@gmail.com>
2024-06-22 21:52:28 +0300

    creating services structure

--- src/exercise/exercise.service.ts
+++ src/exercise/exercise.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
+interface IExerciseService {
+  create(): void;
+  getById(): void;
+  update(): void;
+  delete(): void;
+}
+
 @Injectable()
-export class ExerciseService {}
+export class ExerciseService implements IExerciseService {
+  create(): void {}
+  getById(): void {}
+  update(): void {}
+  delete(): void {}
+}
--- src/repeat/repeat.service.ts
+++ src/repeat/repeat.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
+interface IRepeatService {
+  create(): void;
+  getById(): void;
+  update(): void;
+  delete(): void;
+}
+
 @Injectable()
-export class RepeatService {}
+export class RepeatService {
+  create(): void {}
+  getById(): void {}
+  update(): void {}
+  delete(): void {}
+}
