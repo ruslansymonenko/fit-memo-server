0a968ab67d876f9a2efc301cd84dd008fea5d22f
Ruslan Symonenko <symonenkors@gmail.com>
2024-06-22 21:47:44 +0300

    working on structure of auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
+interface IAuthService {
+  login(): void;
+  register(): void;
+  refreshTokens(): void;
+  createTokens(): void;
+  addRefreshTokenToResponse(): void;
+  removeRefreshTokenFromResponse(): void;
+  validateUser(): void;
+}
+
 @Injectable()
-export class AuthService {}
+export class AuthService implements IAuthService {
+  login(): void {}
+
+  register(): void {}
+
+  refreshTokens(): void {}
+
+  createTokens(): void {}
+
+  addRefreshTokenToResponse(): void {}
+
+  removeRefreshTokenFromResponse(): void {}
+
+  validateUser(): void {}
+}
